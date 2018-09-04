c0803573c645bc5c395f0b4e7820de53372b1e16
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-09-04 17:12:54 +0700

    Initial commit

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -14,11 +14,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt-multiple-app',
+    title: '{{ slugify name }}',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      { hid: 'description', name: 'description', content: '{{ escape description }}' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
